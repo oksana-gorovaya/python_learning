9ab5f6287ff56dd9fa992bdee634755564226058
oksana-gorovaya <ksenia15grv@gmail.com>
2019-05-28 13:56:36 +0300

    Task 2.3 & 2.4 done.

---
+++ DNA_coder.py
@@ -0,0 +1,26 @@
+"""Кодирование осуществляется следующим образом:
+s = 'aaaabbсaa' преобразуется в 'a4b2с1a2', то есть группы одинаковых символов исходной строки заменяются на этот
+символ и количество его повторений в этой позиции строки.
+
+Напишите программу, которая считывает строку, кодирует её предложенным алгоритмом и выводит закодированную
+последовательность на стандартный вывод. Кодирование должно учитывать регистр символов."""
+
+DNA_sample = input().lower()
+DNA_coded = ''
+arr = list(DNA_sample)
+temp_str = ''
+
+index = 0
+for item in arr:
+    if arr[index] != arr[index - 1]:
+        temp_str += f",{item}"
+    else:
+        temp_str += f"{item}"
+    index += 1
+
+temp_array = temp_str.split(',')
+
+for element in temp_array:
+    DNA_coded += f"{element[:1]}{len(element)}"
+
+print(DNA_coded)
