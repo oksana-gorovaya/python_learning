48d8c325182fb74ce9de5057bcf70337fb987c7b
oksana-gorovaya <ksenia15grv@gmail.com>
2019-06-08 16:55:29 +0300

    Input validation check added.

--- average.py
+++ average.py
@@ -6,14 +6,20 @@
 
 На вход программе подаются интервалы, внутри которых всегда есть хотя бы одно число, которое делится на 3.﻿"""
 
-first_number = int(input())
-second_number = int(input())
+first_number = input()
+second_number = input()
+
+try:
+    checking_first_input = int(first_number)
+    checking_second_input = int(second_number)
+except ValueError:
+    print("Enter numbers only")
 
 
 def calculate_average(first_number, second_number):
     counter = 0
     numbers_sum = 0
-    for i in range(first_number, second_number + 1):
+    for i in range(int(first_number), int(second_number) + 1):
         if i % 3 == 0:
             numbers_sum += i
             counter += 1
