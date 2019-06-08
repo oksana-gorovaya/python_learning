a02345d4e56eac05fbe6d54ede9404569ace8c00
oksana-gorovaya <ksenia15grv@gmail.com>
2019-06-08 16:12:24 +0300

    Tasks in section 3.4 done.

---
+++ decode_string.py
@@ -0,0 +1,55 @@
+"""Напишите программу, которая считывает из файла строку, соответствующую тексту, сжатому с помощью кодирования
+повторов, и производит обратную операцию, получая исходный текст.
+L4V7H18O20a9m20R18Q2h12x9P12l15o19B3H17f15q17o10N11U11S13f10q6Q17W14U4p16w3S6M2A1a11r1J12
+
+"""
+import re
+
+
+with open('/home/xenia/Завантаження/dataset_3363_2.txt', 'r') as encoded_file:
+    encoded_string = encoded_file.readline().strip()
+
+
+pattern = '[a-zA-Z]+'
+
+
+def split_input(encoded_string, pattern):
+    data_array = []
+    for item in encoded_string:
+        if re.search(pattern, item):
+            chunk = re.sub(pattern, f",{item}", item)
+            data_array.append(chunk)
+        else:
+            data_array.append(item)
+
+    string_chunks = "".join(data_array)
+    return string_chunks.split(",")
+
+
+def restore_letter_blocks(splited_input, pattern):
+    result = []
+    for item in splited_input:
+        if item != "":
+            letters = []
+            numbers = []
+            for element in item:
+                if re.search(pattern, element):
+                    letters.append(element)
+                else:
+                    numbers.append(element)
+            duplicate_amount = "".join(numbers)
+            letter_block = letters[0] * int(duplicate_amount)
+            result.append(letter_block)
+    return result
+
+
+def decode_final_string(restored_blocks):
+    return "".join(restored_blocks)
+
+
+splited_input = split_input(encoded_string, pattern)
+restored_blocks = restore_letter_blocks(splited_input, pattern)
+initial_string = decode_final_string(restored_blocks)
+
+with open('/home/xenia/Документи/decoded_file.py', 'w') as decoded_file:
+    decoded_file.write(initial_string)
